59b984590b55b808dfa36b113141ad149b8df1df
qingyuano <211720950@qq.com>
2023-10-10 22:16:53 +0800

    update : 添加简历

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -88,6 +88,7 @@ export default function Intro() {
         <a
           className="borderBlack group flex cursor-pointer items-center gap-2 rounded-full border border-solid border-black/10 bg-white px-7 py-3 outline-none transition hover:scale-110 focus:scale-110 active:scale-105 dark:bg-white/10"
           download
+          href="/CV.pdf"
         >
           简历{" "}
           <HiDownload className="opacity-60 transition group-hover:translate-y-1" />
