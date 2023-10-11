2ef80c7ddde09db02880d8ae5d953b8048c44567
chenhu <chenhu@reedsec.com>
2023-10-11 17:48:36 +0800

    fix : 修复暗黑模式图标显示错误

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -4,15 +4,24 @@ import React, { useEffect, useState } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
   const { setTheme, theme } = useTheme();
   useEffect(() => {
-    const themeMedia = window.matchMedia("(prefers-color-scheme: light)");
-    if (themeMedia.matches) {
-      setTheme("light");
-    } else {
-      setTheme("dark");
+    if (theme === "system") {
+      const themeMedia = window.matchMedia("(prefers-color-scheme: light)");
+      if (themeMedia.matches) {
+        setTheme("light");
+      } else {
+        setTheme("dark");
+      }
     }
-  }, [setTheme]);
+    setMounted(true);
+  }, [setTheme, theme]);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <button
       className="fixed bottom-5 right-5 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-2xl backdrop-blur-[0.5rem]  transition-all hover:scale-[1.15] active:scale-105 dark:bg-gray-950"
