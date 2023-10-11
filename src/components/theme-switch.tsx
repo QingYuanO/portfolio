fc5f0660db62555c5a5288d153b352b2b0e0b983
chenhu <chenhu@reedsec.com>
2023-10-11 09:26:22 +0800

    fix : 系统主题导致图标错误

--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -4,15 +4,15 @@ import React, { useEffect, useState } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
 
 export default function ThemeSwitch() {
-  const [mounted, setMounted] = useState(false);
   const { setTheme, theme } = useTheme();
   useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
+    const themeMedia = window.matchMedia("(prefers-color-scheme: light)");
+    if (themeMedia.matches) {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  }, [setTheme]);
   return (
     <button
       className="fixed bottom-5 right-5 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full border border-white border-opacity-40 bg-white bg-opacity-80 shadow-2xl backdrop-blur-[0.5rem]  transition-all hover:scale-[1.15] active:scale-105 dark:bg-gray-950"
